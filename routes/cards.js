7e82df888698566a03d212aaa6e8f970be90d05d
Roman Faritov <romko_1992@mail.ru>
2022-06-14 03:48:25 +0500

    feat: create backend+auth

--- routes/cards.js
+++ routes/cards.js
@@ -20,19 +20,19 @@ router.get('/', getCards);
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum(),
+    cardId: Joi.string().required().alphanum(),
   }),
 }), deleteCard);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum(),
+    cardId: Joi.string().required().alphanum(),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum(),
+    cardId: Joi.string().required().alphanum(),
   }),
 }), dislikeCard);
 
